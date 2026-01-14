6e158b141300b95e73120abd64077bb5b1ac0d1d
Cristian0-Dev <ceverly1@nait.ca>
2026-01-14 11:31:01 -0700

    random

--- 01 - javascript review/examples/01 - break form example/student/js/index.js
+++ 01 - javascript review/examples/01 - break form example/student/js/index.js
@@ -7,3 +7,33 @@ HTML for list topic list item
 </li>
 */
 
+let topicList = document.querySelector(".topics-list")
+let newTopicForm = document.querySelector(".new-topic-form")
+
+
+const addTopicToPage = (topicName, topicListElement) => {
+    let newTopicElement = `<li class="list-group=item">
+        ${topicName}
+    </li>`
+    topicListElement += topicListElement + newTopicElement
+}
+
+
+
+newTopicForm.addEventListener(
+    "submit",
+    (event) => {
+        event.preventDefault()
+        let topicInput = event.target.elements["new-topic"]
+        let newTopic = topicInput.value
+        console.log(newTopic)
+
+        // 4 validation check if empty
+        if (newTopic === "") {
+            topicInput.classList.add("is=invalid")
+        } else {
+            topicInput.classList.remove("is-invalid")
+        }
+    }
+)
+
